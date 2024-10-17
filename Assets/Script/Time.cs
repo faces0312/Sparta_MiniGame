995ac54b24e7dcf21d5559f99b8f8c20f14fa6ab
Hwijun-Yang <didgnlwns8728@naver.com>
2024-10-17 19:29:08 +0900

    1929

--- Assets/Script/Time.cs
+++ Assets/Script/Time.cs
@@ -5,25 +5,28 @@ using UnityEngine.UI;
 
 public class script : MonoBehaviour
 {
+    TimeFlow timeFlow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeFlow = new TimeFlow();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeFlow.Update();
     }
+
     public class TimeFlow
     {
         public Text timeText;
         private float remainingTime = 30.00f;
 
-        void Update()
+        public void Update()
         {
-            if (remainingTime > 0 )
+            if (remainingTime > 0)
             {
                 remainingTime -= Time.deltaTime;
                 timeText.text = "Time: " + remainingTime.ToString("F2") + "seconds";
@@ -31,7 +34,7 @@ public class script : MonoBehaviour
             else
             {
                 timeText.text = "Time: 0.00 seconds";
-            }   
+            }
         }
     }
 }
