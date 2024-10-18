cd6c8b6bc0c8b4c1b23e2c5f2b4e68b1744bb98b
economy937 <economy9895@naver.com>
2024-10-18 16:14:00 +0900

    add

--- Assets/2. BLOCK/Script/GM_Block.cs
+++ Assets/2. BLOCK/Script/GM_Block.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GM_Block : MonoBehaviour
 {
     public int totalLives = 3;
     public int curLives;
     public Text Life;
+    public GameObject RetryPanel;
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1f;
         curLives = totalLives;
         Life.text = "Life :" + curLives.ToString();
     }
@@ -26,9 +29,17 @@ public class GM_Block : MonoBehaviour
         Life.text = "Life : " + curLives.ToString();
     }
 
+    public void RetryPanelOn()
+    {
+        RetryPanel.SetActive(true);
+    }
+
+    
+
     private void GameOver()
     {
         Time.timeScale = 0.0f;
+        RetryPanel.SetActive(true);
     }
 
 }
