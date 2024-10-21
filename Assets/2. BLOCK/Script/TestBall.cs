56d09ecc076665eaa510106f1bab84e0a49b7a77
HyunSeok Suh <suhya990312@gmail.com>
2024-10-21 15:02:32 +0900

    Block_Fix

--- Assets/2. BLOCK/Script/TestBall.cs
+++ Assets/2. BLOCK/Script/TestBall.cs
@@ -5,18 +5,8 @@ using UnityEngine;
 
 public class TestBall : MonoBehaviour
 {
-    //private GM_Block GM_Block;
-    //public int ballCount;
-
-    /*void Start()
-    {
-        GM_Block = FindObjectOfType<GM_Block>();
-    }*/
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //ballCount = GameObject.FindGameObjectsWithTag("Ball").Length; //태그는 변경하기
-
         if (collision.gameObject.tag == "Ground")
         {
             Debug.Log(GM_Block.gm_Block.ball_Num);
@@ -36,17 +26,5 @@ public class TestBall : MonoBehaviour
                 gameObject.SetActive(false);
             }
         }
-    }
-
-    private void ResetBall()
-    {
-        Vector2 pos;
-
-        /*GameObject[] player = GameObject.FindGameObjectsWithTag("Player");*/
-
-        pos = GM_Block.gm_Block.player.transform.position;
-
-        transform.position = new Vector2(pos.x, pos.y+2);
-        gameObject.SetActive(true);
     }
 }
